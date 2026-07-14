fa0fcb61a63973cd69f4e1e0adf9ff162120b800
ARNAV <ARNAV@DESKTOP-MKVNNS0>
2026-07-14 15:42:30 +0530

    Add project files.

---
+++ Modals/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.API.Modals
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public string? Description { get; set; }
+        public int? Quantity { get; set; }
+        public int IsActive { get; set; }
+    }
+}
